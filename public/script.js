28c168f597d3212d1d54706518e05397af98e4aa
imansedky5 <imans.ewaiti@gmail.com>
2020-11-29 07:47:07 +0200

    jjj

--- public/script.js
+++ public/script.js
@@ -1,26 +1,25 @@
 const searchInput = document.getElementById('search-input');
 const container = document.getElementById('container');
 
-const fetchCites = (userInput, callBack1) => {
+const fetchCites = (callBack) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        const parsedResponse = JSON.parse(xhr.responseText);
-        callBack1(parsedResponse, userInput);
+        callBack(JSON.parse(xhr.responseText));
       }
     }
   };
   xhr.open('GET', '/cities', true);
   xhr.send();
 };
-const handelReponse = (callBack2, parsedResponse, userInput) => {
-  const letter = userInput.slice(0, 2).toUpperCase();
+const handelReponse = (parsedResponse, userInput) => {
+  const letter = userInput.slice(0, 1).toUpperCase();
   const autoCompleteChoices = parsedResponse
     .map((el) => el.toUpperCase())
     .filter((el) => el.startsWith(letter))
     .slice(0, 20);
-  callBack2(autoCompleteChoices);
+  return autoCompleteChoices;
 };
 
 const renderCites = (autoCompleteChoices) => {
@@ -33,5 +32,10 @@ const renderCites = (autoCompleteChoices) => {
 
 searchInput.addEventListener('input', (e) => {
   const inputValue = e.target.value;
-  fetchCites(inputValue, () => handelReponse(renderCites));
+  if (inputValue) {
+    fetchCites((data) => renderCites(handelReponse(data, inputValue)));
+    [...container.querySelectorAll('*')].forEach((el) => el.remove());
+  } else {
+    [...container.querySelectorAll('*')].forEach((el) => el.remove());
+  }
 });
